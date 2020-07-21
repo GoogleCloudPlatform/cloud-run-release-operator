4977f03a93f5a9e654039f2f81b977113715e15d
Getulio Valentin Sánchez <valentin2507@gmail.com>
2020-07-21 14:08:51 -0400

    health: Create package to determine health
    This creates a new health package that has a main IsHealthy function to check if
    the metrics for the candidate revision meet the configuration criteria.
    
    Signed-off-by: Getulio Valentin Sánchez <valentin2507@gmail.com>

---
+++ pkg/health/health.go
@@ -0,0 +1,69 @@
+package health
+
+import (
+	"context"
+	"time"
+
+	"github.com/GoogleCloudPlatform/cloud-run-release-operator/internal/metrics"
+	"github.com/GoogleCloudPlatform/cloud-run-release-operator/pkg/config"
+	"github.com/pkg/errors"
+)
+
+// Checker validates that revision is healthy.
+type Checker interface {
+	IsHealthy(ctx context.Context, query metrics.Query, healthCheckOffset time.Duration, metricsChecks []config.Metric) (bool, error)
+}
+
+// Health is the candidate's health status checker.
+type Health struct {
+	Metrics metrics.Metrics
+}
+
+// New initializes a health checker.
+func New(metrics metrics.Metrics) *Health {
+	return &Health{
+		Metrics: metrics,
+	}
+}
+
+// IsHealthy checks if a revision is healthy based on the metrics configuration.
+func (h *Health) IsHealthy(ctx context.Context, query metrics.Query, healthCheckOffset time.Duration, metricsChecks []config.Metric) (bool, error) {
+	healthy := true
+	for _, check := range metricsChecks {
+		var err error
+		switch check.Type {
+		case config.LatencyMetricsCheck:
+			healthy, err = h.checkLatency(ctx, query, healthCheckOffset, check.Percentile, check.Max)
+			break
+		case config.ErrorRateMetricsCheck:
+			healthy, err = h.checkErrorRate(ctx, query, healthCheckOffset, check.Max)
+			break
+		}
+
+		if err != nil {
+			return false, errors.Wrapf(err, "failed to check metrics %q", check.Type)
+		}
+	}
+
+	return healthy, nil
+}
+
+// checkLatency checks that the threshold for latency is not exceeded.
+func (h *Health) checkLatency(ctx context.Context, query metrics.Query, offset time.Duration, percentile, max float64) (bool, error) {
+	latency, err := h.Metrics.Latency(ctx, query, offset, metrics.PercentileToAlignReduce(percentile))
+	if err != nil {
+		return false, errors.Wrap(err, "failed to get latency metrics")
+	}
+
+	return latency <= max, nil
+}
+
+// checkErrorRate checks that the threshold for error rate is not exceeded.
+func (h *Health) checkErrorRate(ctx context.Context, query metrics.Query, offset time.Duration, max float64) (bool, error) {
+	rate, err := h.Metrics.ErrorRate(ctx, query, offset)
+	if err != nil {
+		return false, errors.Wrap(err, "failed to get error rate metrics")
+	}
+
+	return rate <= max, nil
+}
---
+++ pkg/health/health_test.go
@@ -0,0 +1,70 @@
+package health_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/GoogleCloudPlatform/cloud-run-release-operator/internal/metrics"
+	metricsMocker "github.com/GoogleCloudPlatform/cloud-run-release-operator/internal/metrics/mock"
+	"github.com/GoogleCloudPlatform/cloud-run-release-operator/pkg/config"
+	"github.com/GoogleCloudPlatform/cloud-run-release-operator/pkg/health"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIsHealthy(t *testing.T) {
+	metricsMock := metricsMocker.Metrics{}
+	metricsMock.LatencyFn = func(ctx context.Context, query metrics.Query, offset time.Duration, alignReduceType metrics.AlignReduce) (float64, error) {
+		return 500, nil
+	}
+	metricsMock.ErrorRateFn = func(ctx context.Context, query metrics.Query, offset time.Duration) (float64, error) {
+		return 1, nil
+	}
+
+	tests := []struct {
+		name     string
+		query    metrics.Query
+		offset   time.Duration
+		metrics  []config.Metric
+		expected bool
+	}{
+		{
+			name:   "healthy revision",
+			query:  metricsMocker.Query{},
+			offset: 5 * time.Minute,
+			metrics: []config.Metric{
+				{Type: config.LatencyMetricsCheck, Percentile: 99, Max: 750},
+				{Type: config.ErrorRateMetricsCheck, Max: 5},
+			},
+			expected: true,
+		},
+		{
+			name:   "barely healthy revision",
+			query:  metricsMocker.Query{},
+			offset: 5 * time.Minute,
+			metrics: []config.Metric{
+				{Type: config.LatencyMetricsCheck, Percentile: 99, Max: 500},
+				{Type: config.ErrorRateMetricsCheck, Max: 1},
+			},
+			expected: true,
+		},
+		{
+			name:   "unhealthy revision",
+			query:  metricsMocker.Query{},
+			offset: 5 * time.Minute,
+			metrics: []config.Metric{
+				{Type: config.LatencyMetricsCheck, Percentile: 99, Max: 499},
+				{Type: config.ErrorRateMetricsCheck, Max: 0.95},
+			},
+			expected: false,
+		},
+	}
+
+	health := health.New(&metricsMock)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			isHealthy, _ := health.IsHealthy(context.Background(), test.query, test.offset, test.metrics)
+			assert.Equal(t, test.expected, isHealthy)
+		})
+	}
+}
