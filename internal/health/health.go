bd1352e1feb94792a35cd9958dcb7bbeabe1d51f
Getulio Valentin Sánchez <gvso@google.com>
2020-08-19 15:29:01 -0400

    Update function comment documentation
    Signed-off-by: Getulio Valentin Sánchez <gvso@google.com>

--- internal/health/health.go
+++ internal/health/health.go
@@ -54,11 +54,15 @@ type CheckResult struct {
 // actual values are not the same, the diagnosis is Unknown and an error is
 // returned.
 //
-// If the minimum number of requests is not met, then health cannot be
-// determined and diagnosis is Inconclusive.
+// Otherwise, all metrics criteria are checked to determine the diagnosis:
+// healthy, unhealthy, or inconclusive.
 //
-// Otherwise, all metrics criteria are checked to determine whether the revision
-// is healthy or not.
+// If the minimum number of requests is not met, the diagnosis is Inconclusive
+// even though all other criteria are met.
+//
+// However, if any criteria other than the request count is not met, the
+// diagnosis is unhealthy independent on the request count criteria. That is,
+// Unhealthy has precedence over Inconclusive.
 func Diagnose(ctx context.Context, healthCriteria []config.HealthCriterion, actualValues []float64) (Diagnosis, error) {
 	logger := util.LoggerFrom(ctx)
 	if len(healthCriteria) != len(actualValues) {
